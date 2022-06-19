4af172ffbee88ffc6dfae6b262dfac624bca68a4
g.mironov <g.mironov@corp.mail.ru>
2022-06-19 21:33:30 +0300

    v1: mail headers fix

--- mail/mail.go
+++ mail/mail.go
@@ -22,8 +22,10 @@ func (m *Mailer) SendEmail(subj, body string, receivers []string) error {
 	var sb strings.Builder
 	sb.WriteString(fmt.Sprintf("Date: %s\r\n", time.Now().String()))
 	sb.WriteString(fmt.Sprintf("From: %s\r\n", m.User))
+
+	receivers = append(receivers, m.User)
 	sb.WriteString(fmt.Sprintf("To: %s\r\n", strings.Join(receivers, ",")))
-	sb.WriteString(fmt.Sprintf("Bcc: %s\r\n", m.User))
+
 	sb.WriteString(fmt.Sprintf("Subject: %s\r\n", subj))
 	sb.WriteString("MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n")
 	sb.WriteString(body)
@@ -31,6 +33,5 @@ func (m *Mailer) SendEmail(subj, body string, receivers []string) error {
 	msg := []byte(sb.String())
 	auth := smtp.PlainAuth("", m.User, m.Password, host)
 
-	receivers = append(receivers, m.User)
 	return  smtp.SendMail(addr.Address, auth, m.User, receivers, msg)
 }
